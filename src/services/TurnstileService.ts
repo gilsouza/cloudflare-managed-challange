33a1cc4cbe3453695553138396ecbbf5a609d4b3
Gil Souza <gilmarcs@gmail.com>
2025-05-24 10:47:28 -0300

    refactor TurnstileService to enhance logging and improve error handling in script loading and challenge execution

--- src/services/TurnstileService.ts
+++ src/services/TurnstileService.ts
@@ -28,8 +28,6 @@ declare global {
   }
 }
 
-const TURNSTILE_DEFAULT_CONFIG = {};
-
 export class TurnstileService {
   private static instance: TurnstileService;
   private loadPromise: Promise<void> | null = null;
@@ -39,24 +37,36 @@ export class TurnstileService {
   private readonly appearance = "always"; // debug
   private readonly containerId = "#cf-turnstile-container";
 
-  private constructor(private siteKey: string) {}
+  private constructor(private siteKey: string) {
+    console.log("TurnstileService :: constructor");
+  }
 
   public static getInstance(siteKey: string): TurnstileService {
+    console.log("TurnstileService :: getInstance");
     if (!TurnstileService.instance) {
+      console.log("TurnstileService :: getInstance :: new instance");
       TurnstileService.instance = new TurnstileService(siteKey);
     }
     return TurnstileService.instance;
   }
 
   private loadScript(): Promise<void> {
-    if (this.loadPromise) return this.loadPromise;
+    console.log("TurnstileService :: loadScript");
+    if (this.loadPromise) {
+      console.log("TurnstileService :: loadScript :: already loading");
+      return this.loadPromise;
+    }
 
     this.loadPromise = new Promise((resolve, reject) => {
+      console.log("TurnstileService :: loadScript :: start promise");
       const actualScript = document.querySelector(
         'script[src*="challenges.cloudflare.com/turnstile"]'
       );
 
       if (actualScript) {
+        console.log(
+          "TurnstileService :: loadScript :: start promise :: already loaded"
+        );
         actualScript.addEventListener("load", () => resolve());
         actualScript.addEventListener("error", () =>
           reject(new Error("Failed to load Turnstile script"))
@@ -64,14 +74,25 @@ export class TurnstileService {
         return;
       }
 
+      console.log("TurnstileService :: loadScript :: start promise :: create");
+
       const cfTurnstileScript = document.createElement("script");
       cfTurnstileScript.src =
         "https://challenges.cloudflare.com/turnstile/v0/api.js?render=explicit";
       cfTurnstileScript.async = true;
       cfTurnstileScript.defer = true;
-      cfTurnstileScript.onload = () => resolve();
-      cfTurnstileScript.onerror = () =>
+      cfTurnstileScript.onload = () => {
+        console.log(
+          "TurnstileService :: loadScript :: start promise :: onload"
+        );
+        resolve();
+      };
+      cfTurnstileScript.onerror = () => {
+        console.log(
+          "TurnstileService :: loadScript :: start promise :: onerror"
+        );
         reject(new Error("Failed to load Turnstile script"));
+      };
       document.head.appendChild(cfTurnstileScript);
     });
 
@@ -82,14 +103,19 @@ export class TurnstileService {
     action?: string,
     cData?: string
   ): Promise<string> {
+    console.log("TurnstileService :: executeChallenge");
     await this.loadScript();
 
     return new Promise<string>((resolve, reject) => {
+      console.log("TurnstileService :: executeChallenge :: start promise");
       let container = document.querySelector(
         this.containerId
       ) as HTMLDivElement | null;
 
       if (container) {
+        console.log(
+          "TurnstileService :: executeChallenge :: start promise :: container exists"
+        );
         this.widgetId = window.turnstile?.render(container, {
           ...(action && { action }),
           ...(cData && { cData }),
@@ -97,22 +123,40 @@ export class TurnstileService {
           mode: this.mode,
           appearance: this.appearance,
           callback: (token: string) => {
+            console.log(
+              "TurnstileService :: executeChallenge :: render :: callback"
+            );
             resolve(token);
             window.turnstile?.reset(this.widgetId!);
+            console.log("TurnstileService :: executeChallenge :: reseted");
           },
-          "error-callback": () =>
-            reject(new Error("Turnstile challenge error")),
-          "expired-callback": () =>
-            reject(new Error("Turnstile challenge expired")),
+          // @ts-ignore
+          "error-callback": (error) => {
+            console.log(
+              "TurnstileService :: executeChallenge :: render :: error-callback",
+              error
+            );
+            reject(new Error("Turnstile challenge error"));
+          },
+          // @ts-ignore
+          "expired-callback": (error) => {
+            console.log(
+              "TurnstileService :: executeChallenge :: render :: expired-callback",
+              error
+            );
+            reject(new Error("Turnstile challenge expired"));
+          },
         });
       }
 
-      if (this.widgetId === undefined) {
+      if (!this.widgetId) {
+        console.log("TurnstileService :: executeChallenge :: widgetId is null");
         reject(new Error("Turnstile render failed"));
         return;
       }
 
       window.turnstile?.execute(this.widgetId);
+      console.log("TurnstileService :: executeChallenge :: executed");
     });
   }
 }
